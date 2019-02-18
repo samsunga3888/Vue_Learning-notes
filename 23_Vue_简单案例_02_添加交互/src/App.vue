8647b88203a4ebb1639a656559dbb395a14b6fb9
samsunga3888 <samsunga3888@gmail.com>
2019-02-18 13:30:02 +0800

    部分代码更新

---
+++ 23_Vue_简单案例_02_添加交互/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+	
+	<div>
+	  <header class="site-header jumbotron">
+	    <div class="container">
+	      <div class="row">
+	        <div class="col-xs-12">
+	          <h1>请发表对Vue的评论</h1>
+	        </div>
+	      </div>
+	    </div>
+	  </header>
+	  <div class="container">
+		  <!--  02_5.将methods里面的新增的方法函数传递给Add.vue组件 -->
+		  <Add :addComment="addComment"/>
+		  <List :comments='comments'/>
+	  </div>
+	</div>
+</template>
+
+<script>
+	
+	import Add from './components/Add.vue'
+	import List from './components/List.vue'
+	
+	export default{
+		
+		data(){
+			return{
+				comments:[
+					{
+						name:'BOB',
+						content:'Vue还不错'
+					},
+					{
+						name:'Cat',
+						content:'Vue So Easy'
+					},
+					{
+						name:'BZ',
+						content:'Vue So So'
+					}
+				]
+			}
+		},
+		
+		methods:{
+			//02_4.编写添加评论方法
+			addComment(comment){
+				//unshift作用，在comments最前面增加一条评论数据
+				this.comments.unshift(comment)
+			}
+		},
+		
+		
+		components:{
+			Add,
+			List,
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ 23_Vue_简单案例_02_添加交互/src/components/Item.vue
@@ -0,0 +1,51 @@
+<template>
+	<!--  09.编写各种评论代码 -->
+	<li class="list-group-item">
+	  <div class="handle">
+	    <a href="javascript:;">删除</a>
+	  </div>
+		<!-- 14_4.动态显示每一条评论内容comment-->
+	  <p class="user"><span >{{comment.name}}</span><span>说:</span></p>
+	  <p class="centence">{{comment.content}}</p>
+	</li>
+</template>
+
+<script>
+	export default{
+		//14_3.通过props接收来自父组件List.vue传过来的数值。
+		props:{//第二种写法，指定属性名和属性值的类型。第一种写法props:['comment']。
+			comment:Object
+		}
+	}
+</script>
+
+<style>
+	/*12_2.拆分index.html页面引入的css样式*/
+	li {
+	  transition: .5s;
+	  overflow: hidden;
+	}
+	
+	.handle {
+	  width: 40px;
+	  border: 1px solid #ccc;
+	  background: #fff;
+	  position: absolute;
+	  right: 10px;
+	  top: 1px;
+	  text-align: center;
+	}
+	
+	.handle a {
+	  display: block;
+	  text-decoration: none;
+	}
+	
+	.list-group-item .centence {
+	  padding: 0px 50px;
+	}
+	
+	.user {
+	  font-size: 22px;
+	}
+</style>
