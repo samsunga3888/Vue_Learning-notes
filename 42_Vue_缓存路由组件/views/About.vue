102a07fc05bc73872bc2d9d114322844ae5ee174
samsunga3888 <samsunga3888@gmail.com>
2019-03-14 08:04:54 +0800

    部分代码更新

---
+++ 42_Vue_缓存路由组件/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>About</h2>
+		<!--01.新建input演示路由缓存效果，用于与后台数据交互实时性低的情况下。-->
+		<input type="text" />
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		
+	}
+</script>
+
+<style>
+</style>
